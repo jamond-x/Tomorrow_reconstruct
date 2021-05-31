8432c7b2266c8aa431cdd34f72ee9e121030ac1f
jamond-x <x2592253600@163.com>
2021-06-01 00:49:01 +0800

    docs&feat:初步完善登录功能，添加Axios文件

--- front_end/src/signIn_.js
+++ front_end/src/signIn_.js
@@ -1,18 +1,84 @@
 
-
 Vue.component('sign-in',{
-    template:`
-        <div class='container'>
-            <div class='container-head'></div>
-        </div>
-    `
+    template:'#sign-in',
+    data:function(){
+        return{
+            email_p:'',
+            password_p:''
+        }
+    },
+    methods:{
+        submit:function(){
+            this.$emit('submitting')
+        }
+    }
+})
+
+Vue.component('myinput',{
+    template:'#myinput',
+    data:function(){
+        return {
+            email:'',
+            password:''
+        }
+    },
+    watch:{
+        email:function(newVal){
+            this.$emit('reception_email',this.email)
+        },
+        password:function(newVal){
+            this.$emit('reception_password',this.password)
+        }
+    }
 })
 
 let vm = new Vue(
     {
         el:'#app',
         data:{
-            name:'Gainni Hueng'
+            name:'Gainni Hueng',
+            email:'',
+            password:''
+        },
+        methods:{
+            handle_email:function(val){
+                this.email = val;
+            },
+            handle_password:function(val){
+                this.password = val
+            },
+            submitting:function(){
+                this.post()
+            },
+            post:function(){
+                axios.get('/server/data_/myJson.json')
+                .then(
+                    res => {
+                        this.handle_res(res)
+                    }
+                )
+                .catch(
+                    err => {
+                        console.log(err);
+                        this.handle_res(err)
+                    }
+                )
+            },
+            handle_res:function(data_){
+                if(data_.status === 200){
+                    if(data_.data.code === 0){
+                        console.log("登陆成功");
+                        // 路径跳转
+                    }else if(data_.data.code === 1){   
+                        console.log("密码错误");
+                    }else if(data_.data.code === 2){
+                        // do something
+                    }
+                }else{  //  具体加 else if ( status == 404)  等
+                    console.log('服务器相应失败');
+                }
+                console.log(data_);
+            }
         }
     }
 )
